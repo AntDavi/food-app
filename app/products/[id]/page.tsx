e696c6dab815660c4544750e7bbd50ee51e98c67
AntDavi <ant.davi07@gmail.com>
2024-05-06 22:10:44 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,47 @@
+import { db } from "@/app/_lib/prisma";
+import ProductsImage from "./_components/produtsImage";
+import ProductDetails from "./_components/productDetails";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  return (
+    <div>
+      <ProductsImage product={product} />
+
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default ProductPage;
